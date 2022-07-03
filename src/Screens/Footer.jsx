faa92c5b4beca46f0768ddc186ab972cd9ba374a
Nurudeen Ahmed <86122181+nurudeen06@users.noreply.github.com>
2022-07-03 11:34:59 +0100

    Update now

---
+++ src/Screens/Footer.jsx
@@ -0,0 +1,66 @@
+const Footer = () => {
+    return(
+        <section className="footer">
+            <div className="first-footer">
+                <div className="top-footer">
+                <h1>Newtonian Solution</h1>
+                <p>xx, Niger Road, Ilorin, Kwara State, Nigeria.</p>
+            </div>
+            <div className="bottom-footer">
+                <div className="footer-category">
+                    <div className="title">
+                        <h2>Product</h2>
+                        <>
+                            <li>Featured</li>
+                            <li>Technology</li>
+                            <li>Performance & Rating</li>
+                            <li>Quality Management</li>
+                        </>
+                    </div>
+                </div>
+                <div className="footer-category">
+                    <div className="title">
+                        <h2>Support</h2>
+                        <>
+                            <li>Help Center</li>
+                            <li>Ticket Support</li>
+                            <li>Contact Us</li>
+                            <li>Community</li>
+                        </>
+                    </div>
+                </div>
+                <div className="footer-category">
+                    <div className="title">
+                        <h2>Company</h2>
+                        <>
+                            <li>About Us</li>
+                            <li>Leadership</li>
+                            <li>Careers</li>
+                            <li>News & Article</li>
+                            <li>Partner</li>
+                        </>
+                    </div>
+                </div>
+                <div className="footer-category">
+                    <div className="title">
+                        <h2>Legal</h2>
+                        <>
+                            <li>Term of use</li>
+                            <li>Privacy Policy</li>
+                            <li>Cookie Policy</li>
+                        </>
+                    </div>
+                </div>
+            </div>
+            </div>
+            <hr/>
+            <div className="last-footer">
+                <>
+                    <p>Copyright 2022 Newtonian's Solution, All right reserved.</p>
+                </>
+            </div>
+            
+        </section>
+    )
+}
+export default Footer;
